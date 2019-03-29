69427c5543122c15df2727ea64e7420b0e2d277b
Hien Pham <45201848+hootpy@users.noreply.github.com>
2019-03-29 17:11:40 +0700

    homework

--- Homework/session 1/js-practices/Practice2/sortPractice.js
+++ Homework/session 1/js-practices/Practice2/sortPractice.js
@@ -1,8 +1,13 @@
 'use strict'
 
 function sort(input) {
-  let sorted_list = []
-
+  let sorted_list = [];
+  while (input.length !== 0){
+    let min = Math.min.apply(null,input);
+    sorted_list.push(min)
+    let index = input.indexOf(min);
+    input.splice(index,1);
+  }
   return sorted_list; // Remove this line and change to your own algorithm
 }
 
--- Homework/session 1/js-practices/Practice3/generatePractice.js
+++ Homework/session 1/js-practices/Practice3/generatePractice.js
@@ -1,13 +1,62 @@
 'use strict'
 
 function generate(testLengthArray){
-  return Array.from({length : testLengthArray.length})
-    .map(item => ({
-      input: Array.from({length: item}).map(item => []),
-      target: 0,
-      output: -1
+    let arrayData = [];
+    let generateTypeList = [0,1,2,3];
+
+    testLengthArray.forEach(function (arrayLength) {
+        for(let k = 0; k < arrayLength; k++){
+            let generateType =  generateTypeList[Math.floor(Math.random() * generateTypeList.length)]
+            if(generateType === 0){
+                arrayData.push({
+                    input: [],
+                    output: -1,
+                })
+            } else if(generateType === 1){
+                let inputArray = [];
+                inputArray.push(Math.floor(Math.random()*20000 - 10000))
+                arrayData.push({
+                    input: inputArray,
+                    target: inputArray[0],
+                    output: 0
+                })
+            } else if(generateType === 2){
+                let inputArray = [];
+                inputArray.push(Math.floor(Math.random()*20000 - 10000))
+                arrayData.push({
+                    input: inputArray,
+                    target: inputArray[inputArray.length -1],
+                    output: inputArray.length -1
+                })
+            } else if(generateType === 3){
+                let inputArray = [];
+                inputArray.push(Math.floor(Math.random()*20000 - 10000))
+                while (true){
+                    var index = Math.random()*(inputArray.length)
+                    if(index !== 0 && index !== inputArray.length -1){
+                        arrayData.push({
+                            input: inputArray,
+                            target: inputArray[index],
+                            output: index,
+                        })
+                        break;
+                    }
+                }
+
+            }
+
+
+        }
     })
-  ); // Remove this line and change to your own algorithm
+     return arrayData;
 }
+//   return Array.from({length : testLengthArray.length})
+//     .map(item => ({
+//       input: Array.from({length: item}).map(item => []),
+//       target: 0,
+//       output: -1
+//     })
+//   ); // Remove this line and change to your own algorithm
+// }
 
 module.exports = generate
